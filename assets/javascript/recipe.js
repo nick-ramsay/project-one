e9257ba36a0ee7f7d43d811457671d3468076e7f
Akhil <akhilvijayan004@gmail.com>
2019-09-14 20:54:02 +1000

    update recipe js

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -1,4 +1,5 @@
 function recipeData() {
+    $("#recipesOption #table").html("");
     var searchRecipe = $("#recipesOption #userInput").val();
     var recipeUrl = "https://api.edamam.com/search?q=" + searchRecipe + "&app_id=cc84332e&app_key=7305bef5a7c92a751b9bfdf0b0ef0c63&from=0&to=3&calories=591-722&health=alcohol-free";
     $.ajax({
