c35b0dccd09e535c8e6186dab0527e7abda5c803
Akhil <akhilvijayan004@gmail.com>
2019-09-15 13:34:18 +1000

    update style of recipe content

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -11,15 +11,15 @@ function recipeData() {
             var wrapper = $('<div class="col-md-12">');
             var card = $('<div class="card">').appendTo(wrapper);
             var row1 = $('<div class="row">').appendTo(card);
-            var leftCol = $('<div class="col-md-4" id="userimg">').appendTo(row1);
+            var leftCol = $('<div class="col-md-4" id="recipeImg">').appendTo(row1);
             var rightCol = $('<div class="col-md-8 px-3">').appendTo(row1);
-            var decsription = $('<div class="details">').appendTo(rightCol);
+            var decsription = $('<div class="recipeDetails">').appendTo(rightCol);
             var label = $('<h1>' + response.hits[i].recipe.label + '</h1>').appendTo(decsription);
             var cal = $('<h2>Calories: ' + Math.round(response.hits[i].recipe.calories) + '</h2>').appendTo(label);
             var health = $('<h5>Health Label: ' + response.hits[i].recipe.healthLabels + '</h5>').appendTo(label);
             var row2 = $('<div class="row">').appendTo(card);
             var col = $('<div class="col-md-12">').appendTo(row2);
-            var ingDecsription = $('<div class="ingredient">').appendTo(col);
+            var ingDecsription = $('<div class="recipeIngredient">').appendTo(col);
             var ingredient = $('<p>Ingredients : ' + response.hits[i].recipe.ingredientLines + '</p>').appendTo(ingDecsription);
             $('<img>')
                 .width('340px')
